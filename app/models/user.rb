928766fbb031f5d939d984ba43274512adecc229
EC2 Default User <ec2-user@ip-172-31-20-242.ap-northeast-1.compute.internal>
2023-01-15 10:13:32 +0000

    Add

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :introduction, presence: true
-  validates :image, presence: true
+  # validates :image, presence: true
          
   has_many :books, dependent: :destroy
   
